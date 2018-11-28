95db3bc63ebc28d79e1e2596f4bb0dceb4a8a908
Nawt Robar <nawtrobar@posteo.de>
2018-11-28 00:26:34 -0500

    conver for one input format and imperial units

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var imported = document.createElement('script');
+imported.src='convEngine.js'
+document.head.appendChild(imported);
+
 var app = new Vue({
 	el: '#app',
 	data:{
@@ -5,16 +9,19 @@ var app = new Vue({
 		input:"",
 		inputArray:"",
 		inVal:5,
-		inUnit:"miles",
-		outUnit:"feet",
+		inUnit:"mi",
+		outUnit:"ft",
 		outVal:0,
+		exp:0,
 	},
 	methods:{
-		dump: function(){
-			this.inputArray=this.input.split(' ');
-		},
 		convert: function(){
-			this.output=this.outVal + " " + this.outUnit;
+			this.inputArray=this.input.split(' ');
+			this.inVal = this.inputArray[0];
+			this.inUnit = this.inputArray[1];
+			this.outUnit= this.inputArray[3];
+			globalOutput=imperialDistToImperialDist(this.inVal, this.exp, this.inUnit, this.outUnit);
+			this.output = globalOutput.val + " " + globalOutput.units;
 		},
 		clear: function(){
 			this.output="";
