7320113777aea2ef13239acf17e3e394572b3402
Nawt Robar <nawtrobar@posteo.de>
2018-12-03 18:05:26 -0500

    improve error handling, add about

--- src/app.js
+++ src/app.js
@@ -14,16 +14,15 @@ var app = new Vue({ 		//web app object
 	methods:{
 		inValOnChange: function(){      //handle changes to the value input field
 			if(isNaN(app.inVal)){  				//if not a number output, error
-				document.getElementById("inVal").classList.add('inputError');
 				errorHandler(1);
 			}
 			else if(app.inVal<0){ 				//if less than 0 zero, error
-				document.getElementById("inVal").classList.add('inputError');
 				errorHandler(4);
 			}
 			else {
 				document.getElementById("inVal").classList.remove('inputError');
 				this.inValError="";
+				this.outputError="";
 			}
 		},
 		inUnitOnChange: function(){
@@ -46,13 +45,7 @@ var app = new Vue({ 		//web app object
 			this.inVal=null;
 			this.inputArray="";
 			this.outVal=null;
-			this.inValError="";
-			this.outputError="";
-			this.inUnitError="";
-			this.outUnitError="";
-			document.getElementById("outUnit").classList.remove('inputError');
-			document.getElementById("inUnit").classList.remove('inputError');
-			document.getElementById("inVal").classList.remove('inputError');
+			errorHandler(0);
 		},
 
 	},
@@ -68,57 +61,72 @@ var app = new Vue({ 		//web app object
 function errorCheck(){ //call error handler with appropriate code and return error status
 	var isError=false;
 	if(isNaN(app.inVal)){
-		document.getElementById("inVal").classList.add('inputError');
 		isError=true;
 		errorHandler(1);
 	}
 	else document.getElementById("inVal").classList.remove('inputError');
 	if(app.inVal<0){
-		document.getElementById("inVal").classList.add('inputError');
 		isError=true;
 		errorHandler(4);
 	}
 	else document.getElementById("inVal").classList.remove('inputError');
 	if(!isUnit(app.inUnit)){
-		document.getElementById("inUnit").classList.add('inputError');
 		isError=true;
 		errorHandler(2);
 	}
 	else {
 		document.getElementById("inUnit").classList.remove('inputError');
+		app.inUnit=isUnit(app.inUnit);
 		app.inUnitError="";
 	}
 	if(!isUnit(app.outUnit)){
-		document.getElementById("outUnit").classList.add('inputError');
 		isError=true;
 		errorHandler(3);
 	}	
 	else {
 		document.getElementById("outUnit").classList.remove('inputError');
+		app.outUnit=isUnit(app.outUnit);
 		app.outUnitError="";
 	}
-
 	return isError;
 }
 
 function errorHandler(code) { //output error messages
 	switch(code){
-		case 0:
-			app.inValError="";
+		case 0: // No Error clear all error messages
+			app.inValError = app.inUnitError = app.outUnitError = app.outputError="";
+			document.getElementById("outUnit").classList.remove('inputError');
+			document.getElementById("inVal").classList.remove('inputError');
+			document.getElementById("inUnit").classList.remove('inputError');
 			break;	
-		case 1:
+		case 1: //inVal not a number 
+			app.output="";
 			app.inValError="invalid input";
+			app.outputError="only digits and periods in value field"
+			document.getElementById("inVal").classList.add('inputError');
 			break;
-		case 2:
-			app.inUnitError="invalid unit";
+		case 2: //inUnit invalid
+			document.getElementById("inUnit").classList.add('inputError');
+			app.inUnitError="invalid input";
 			break;
-		case 3:
+		case 3: //outUnit invalid
+			document.getElementById("outUnit").classList.add('inputError');
 			app.outUnitError="invalid unit";
 			break;
-		case 4: 
+		case 4:  //inVal less than 
+			app.output="";
 			app.inValError="less than 0";
+			app.outputError="positive values only"
+			document.getElementById("inVal").classList.add('inputError');
 			break;
-		default:
+		case 5: //incompatible units
+			document.getElementById("inUnit").classList.add('inputError');
+			document.getElementById("outUnit").classList.add('inputError');
+			app.inUnitError="incompatible";
+			app.UnitError="incompatible units";
+			app.outputError="incompatible units";
+
+		default: //some bad input
 			app.outputError="unknown error";
 			break;
 	}
@@ -127,12 +135,22 @@ function errorHandler(code) { //output error messages
 
 function isUnit(unit){ //verfiy unit input validity
 	for(i=0;i<metricbig.length; i++)
-		if(unit === metricbig[i][0] || unit === metricbig[i][1]+"meters") return true;
-
-	for(i=0;i<imperialDistances.length; i++)
-		if(unit === imperialDistances[i][0] || unit === imperialDistances[i][1]) return true;
+		if(unit === metricbig[i][0] + "m" || unit === metricbig[i][1]+"meters" || unit === metricbig[i][1]+"meter") {		
+			return metricbig[i][0] +"m";
+		}
+	for(i=0;i<impDistSpell.length; i++){
+		for(j=0; j<impDistSpell[i].length; j++){
+			if(unit === impDistSpell[i][j])	return impDistSpell[i][0];
+		}
+	}
 	return false;
 }
 
-
+var impDistSpell = [
+	["in", "inch", "inches", "\""],
+	["ft", "foot", "feet", "\'"],
+	["yd", "yard", "yards", "yds"],
+	["mi", "mile", "miles", "moles"],
+	["leage", "league", "leagues", "leges"]
+]
 
